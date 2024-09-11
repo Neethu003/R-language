f20b8c42027f8da61bd95998d7e6557c6d4ee623
Neethu.K <neethukumarswamy417@gmail.com>
2024-09-11 22:32:34 +0530

    Add files via upload
    Taking input from the user in R language

---
+++ Taking input from the user.R
@@ -0,0 +1,9 @@
+{
+val1=as.integer(readline("enter a value:"))
+val2=as.integer(readline("enter a value:"))
+}
+num1=as.integer(readline("enter a number"))
+class(val1)
+print("hello")
+paste("hii","everyone")
+paste("hello","world",sep = "!!")
